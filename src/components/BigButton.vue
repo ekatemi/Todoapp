9b0eac7f956179d6df6e5b3247706f7a61067488
ekatemi <ekatemi@gmail.com>
2022-11-01 23:42:39 +0100

    fix color

--- src/components/BigButton.vue
+++ src/components/BigButton.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="d-grid">
     <button
-    type="submit"
+      type="submit"
       class="btn btn-primary btn-lg rounded-pill data-bs-toggle='button' form-control"
     >
       <slot></slot>
@@ -10,10 +10,12 @@
 </template>
 <style scoped>
 .btn-primary {
-  background: rgb(131,58,180);
-background: linear-gradient(90deg, rgba(131,58,180,1) 0%, rgba(253,29,29,1) 44%, rgba(252,176,69,1) 100%);
+  background: rgb(131, 58, 180);
+  background: linear-gradient(
+    90deg,
+    rgba(131, 58, 180, 1) 0%,
+    rgba(253, 29, 29, 1) 44%,
+    rgba(252, 176, 69, 1) 100%
+  );
 }
 </style>
-
-
-
